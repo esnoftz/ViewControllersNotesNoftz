190c1ddc24e52155092f3f04cda8d3a43c47ebf1
Evangeline Noftz <esnoftz@gmail.com>
2024-09-23 08:15:37 -0500

    Initial Commit

---
+++ ViewControllersNotesNoftzTests/ViewControllersNotesNoftzTests.swift
@@ -0,0 +1,17 @@
+//
+//  ViewControllersNotesNoftzTests.swift
+//  ViewControllersNotesNoftzTests
+//
+//  Created by EVANGELINE NOFTZ on 9/23/24.
+//
+
+import Testing
+@testable import ViewControllersNotesNoftz
+
+struct ViewControllersNotesNoftzTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ ViewControllersNotesNoftzUITests/ViewControllersNotesNoftzUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  ViewControllersNotesNoftzUITestsLaunchTests.swift
+//  ViewControllersNotesNoftzUITests
+//
+//  Created by EVANGELINE NOFTZ on 9/23/24.
+//
+
+import XCTest
+
+final class ViewControllersNotesNoftzUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
